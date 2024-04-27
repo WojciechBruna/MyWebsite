5047ce013da50f8d711205224eaad16939e52719
WojciechBruna <wojciech.bruna@gmail.com>
2024-04-27 12:17:58 +0200

    New photo

--- java.js
+++ java.js
@@ -5,27 +5,8 @@ const hideMenu = () => {
     three.classList.remove('active-menu');
     menuIconWrap.classList.remove('active-menu');
 }
-const nav_container_a_tags = document.querySelectorAll('.navcontainer a');
 
-const menuLinks = [...document.querySelectorAll('navcontainer ul li a')];
+const menuLinks = [...document.querySelectorAll('.wrapper ul li a')];
 menuLinks.forEach(a=> {
     a.addEventListener('click, hideMenu');
 });
-
-
-
-
-
-
-
-// 
-const            nav_chbox = document.querySelector('input[id="nav"]');
-const nav_container_a_tags = document.querySelectorAll('.nav-container a');
- 
-window.onload = () => {
-  nav_container_a_tags.forEach(a_tag => {
-    a_tag.addEventListener('click', () => {
-      nav_chbox.checked = false;    
-    })
-  })  
-}
